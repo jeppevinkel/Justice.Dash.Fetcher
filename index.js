37264dcae73c1e2365b608fb066848ff7bab9f1a
Jeppe Beier <jeppevinkel@gmail.com>
2024-02-25 20:01:51 +0100

    Formatting

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
-const { DateTime } = require('luxon');
+const {DateTime} = require('luxon');
 const fs = require('fs').promises;
 const querystring = require('querystring');
 const Openai = require('openai');
 require('dotenv').config();
 
 const openai = new Openai({
-    apiKey: process.env.OPENAI_API_KEY
-})
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 const days = ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'];
 
@@ -39,7 +39,12 @@ function fetchFoodData() {
      */
     const menuItems = [];
 
-    axios.default.get('https://www.shop.foodandco.dk/api/WeeklyMenu', { params: { restaurantId: 1089, languageCode: 'da-DK' } }).then(res => {
+    axios.default.get('https://www.shop.foodandco.dk/api/WeeklyMenu', {
+        params: {
+            restaurantId: 1089,
+            languageCode: 'da-DK',
+        },
+    }).then(res => {
         /**
          * @type {MenuResponse}
          */
@@ -54,8 +59,8 @@ function fetchFoodData() {
                 type: 'Dagens varme ret',
                 menu: 'Ingenting',
                 friendlyUrl: '/gf/undefined',
-                image: 'https://images.foodandco.dk/Cache/7000/26db3d14e906a285ea7351e22a11617c.jpg'
-            }
+                image: 'https://images.foodandco.dk/Cache/7000/26db3d14e906a285ea7351e22a11617c.jpg',
+            };
 
             menuItems.push({
                 day: days[dayIdx],
@@ -66,8 +71,14 @@ function fetchFoodData() {
         }
 
         // noinspection JSAnnotator
-        let nextMonday = DateTime.fromISO(data.firstDateOfWeek).plus({ days: 7 });
-        axios.default.get('https://www.shop.foodandco.dk/api/WeeklyMenu', { params: { restaurantId: 1089, languageCode: 'da-DK', date: `${nextMonday.toFormat('yyyy-MM-dd')}` } }).then(async res => {
+        let nextMonday = DateTime.fromISO(data.firstDateOfWeek).plus({days: 7});
+        axios.default.get('https://www.shop.foodandco.dk/api/WeeklyMenu', {
+            params: {
+                restaurantId: 1089,
+                languageCode: 'da-DK',
+                date: `${nextMonday.toFormat('yyyy-MM-dd')}`,
+            },
+        }).then(async res => {
             const data = res.data;
 
             for (const dayIdx in data.days) {
@@ -79,8 +90,8 @@ function fetchFoodData() {
                     type: 'Dagens varme ret',
                     menu: 'Ingenting',
                     friendlyUrl: '/gf/undefined',
-                    image: 'https://images.foodandco.dk/Cache/7000/26db3d14e906a285ea7351e22a11617c.jpg'
-                }
+                    image: 'https://images.foodandco.dk/Cache/7000/26db3d14e906a285ea7351e22a11617c.jpg',
+                };
 
                 menuItems.push({
                     day: days[dayIdx],
@@ -91,9 +102,9 @@ function fetchFoodData() {
             }
 
             try {
-                await fs.mkdir('../site/data', { recursive: true });
+                await fs.mkdir('../site/data', {recursive: true});
             } catch (e) {
-                console.error(e)
+                console.error(e);
             }
             await fs.writeFile('../site/data/menu.json', JSON.stringify(menuItems, null, 4)).then(() => console.log('done'));
             await fs.writeFile('../site/data/menu.js', `var menu = ${JSON.stringify(menuItems)}`).then(() => console.log('done'));
@@ -111,7 +122,7 @@ setInterval(fetchFoodData, 21600000);
  * @param {string[]} foodTypes
  * @returns {Promise<void>}
  */
-async function checkFoodContents(foodTypes){
+async function checkFoodContents(foodTypes) {
     /**
      * @type {Array.<{day: string, date: string, foodName: string, foodContents: undefined|string[]}>}
      */
@@ -120,7 +131,7 @@ async function checkFoodContents(foodTypes){
         const rawJson = await fs.readFile('../site/data/menu.json', {encoding: 'utf-8'});
         menuItems = JSON.parse(rawJson);
     } catch (e) {
-        console.error(e)
+        console.error(e);
     }
 
     for (const menuItem of menuItems.filter(it => it.foodContents === undefined)) {
@@ -130,14 +141,14 @@ async function checkFoodContents(foodTypes){
                 messages: [
                     {
                         role: 'system',
-                        content: `Din opgave er at afgøre om der er ${foodType} i denne ret. Hvis den indeholder ${foodType} skal du svare med "ja" og ikke andet. Hvis ikke den indeholder ${foodType} skal du svare med "nej" og ikke andet.`
+                        content: `Din opgave er at afgøre om der er ${foodType} i denne ret. Hvis den indeholder ${foodType} skal du svare med "ja" og ikke andet. Hvis ikke den indeholder ${foodType} skal du svare med "nej" og ikke andet.`,
                     },
                     {
                         role: 'user',
-                        content: `Retten hedder "${menuItem.foodName}"`
-                    }
+                        content: `Retten hedder "${menuItem.foodName}"`,
+                    },
                 ],
-                model: 'gpt-4-0613'
+                model: 'gpt-4-0613',
             });
 
             const response = completion.choices[0].message.content.toLowerCase();
@@ -161,11 +172,11 @@ setInterval(checkFoodContents, 7200000, ['fisk', 'svinekød', 'kød']);
 async function getNetatmo() {
     try {
         try {
-            await fs.mkdir('./secret', { recursive: true });
+            await fs.mkdir('./secret', {recursive: true});
         } catch (e) {
-            console.error(e)
+            console.error(e);
         }
-        let netatmoCredentials = JSON.parse(await fs.readFile('./secret/netatmoCreddentials.json', { encoding: 'utf-8' }));
+        let netatmoCredentials = JSON.parse(await fs.readFile('./secret/netatmoCreddentials.json', {encoding: 'utf-8'}));
 
         const current_time = Date.now();
 
@@ -203,11 +214,11 @@ async function getNetatmo() {
         console.log('rainValue', rainValue);
 
         try {
-            await fs.mkdir('../site/data', { recursive: true });
+            await fs.mkdir('../site/data', {recursive: true});
         } catch (e) {
-            console.error(e)
+            console.error(e);
         }
-        fs.writeFile('../site/data/rain.json', JSON.stringify({ rainValue: rainValue ?? 0 })).then(() => console.log('rain done'));
+        fs.writeFile('../site/data/rain.json', JSON.stringify({rainValue: rainValue ?? 0})).then(() => console.log('rain done'));
     } catch (error) {
         console.log(error);
     }
